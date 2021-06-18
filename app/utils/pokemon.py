069a186eac6f90d6742a254460731356a734ffbf
Anton Pirker <anton@ignaz.at>
2021-06-18 16:58:35 +0200

    Trying to fix bullet lists.

--- app/utils/pokemon.py
+++ app/utils/pokemon.py
@@ -1,38 +1,36 @@
 import requests
 import aiohttp
+import asyncio
+
+import time
 
 import logging
 logger = logging.getLogger(__name__)
 
 
-MAX_POKEMON = 151
+MAX_POKEMON = 50
 NUM_OF_POKEMON_TO_GET = None or MAX_POKEMON
 
-POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
+#POKE_API_URL = 'https://pokeapi.co/api/v2/pokemon/'
+POKE_API_URL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&titles=pokemon'
 
 
 def get_pokemon_sync():
     pokemons = []
-
     for number in range(1, NUM_OF_POKEMON_TO_GET+1):
-        pokemon_url = f'{POKE_API_URL}{number}'
-        resp = requests.get(pokemon_url)
-        pokemon = resp.json()
-        logger.debug(pokemon['name'])
-        pokemons.append(pokemon['name'])
+        time.sleep(0.1)
+        logger.debug(number)
+        pokemons.append('x')
 
-    return pokemons
+    return pokemons 
 
+async def async_sleep(number):
+    await asyncio.sleep(0.1)
+    logger.debug(number)    
+    return 'x'
 
 async def get_pokemon_async():
-    pokemons = []
-
-    async with aiohttp.ClientSession() as session:
-        for number in range(1, NUM_OF_POKEMON_TO_GET+1):
-            pokemon_url = f'{POKE_API_URL}{number}'
-            async with session.get(pokemon_url) as resp:
-                pokemon = await resp.json()
-                logger.debug(pokemon['name'])
-                pokemons.append(pokemon['name'])
-
-    return pokemons
+    pokemons, _ = await asyncio.wait(
+        [async_sleep(number) for number in range(1, NUM_OF_POKEMON_TO_GET+1)]
+    )   
+    return [ pokemon.result for pokemon in pokemons]
