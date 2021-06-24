e8325cbde75961aaacf38253800dbf57f0aeba79
Anton Pirker <anton@ignaz.at>
2021-06-24 16:35:04 +0200

    using pgpass

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,7 +9,8 @@ echo "Test Database"
 #psql "postgres://antonpirker:e44ccd99e65b71bbaf13913e85e9063463fab1d3fd122d58692dc02dcede@postgres12.wdpr.run:5432/antonpirker" -c "select version();"
 
 echo "customers-postgres-12.c0ivpkyb3o26.eu-central-1.rds.amazonaws.com:5432:postgres:wdprcustomeradm:AqsI]IBvS[you[zK8z^=mOb-+gQ1$p<s}9=ku}" > ~/.pgpass
-echo "postgres12.wdpr.run:5432:antonpirker:antonpirker:e44ccd99e65b71bbaf13913e85e9063463fab1d3fd122d58692dc02dcede" >> ~/.pgpass
+echo "\npostgres12.wdpr.run:5432:antonpirker:antonpirker:e44ccd99e65b71bbaf13913e85e9063463fab1d3fd122d58692dc02dcede" >> ~/.pgpass
+chmod 600 ~/.pgpass
 
 echo "cat .pgpass"
 cat ~/.pgpass
