cbdde2e852b00bc311e28db8f14b354109ed9075
Anton Pirker <anton@ignaz.at>
2021-06-24 16:28:36 +0200

    using pgpass

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -11,7 +11,10 @@ echo "Test Database"
 echo "customers-postgres-12.c0ivpkyb3o26.eu-central-1.rds.amazonaws.com:5432:postgres:wdprcustomeradm:AqsI]IBvS[you[zK8z^=mOb-+gQ1$p<s}9=ku}" > ~/.pgpass
 echo "postgres12.wdpr.run:5432:antonpirker:antonpirker:e44ccd99e65b71bbaf13913e85e9063463fab1d3fd122d58692dc02dcede" >> ~/.pgpass
 
-psql "postgres://wdprcustomeradm:AqsI]IBvS[you[zK8z^=mOb-+gQ1$p<s}9=ku}@customers-postgres-12.c0ivpkyb3o26.eu-central-1.rds.amazonaws.com:5432/postgres" -c "select * from information_schema.schemata;"
+echo "cat .pgpass"
+cat ~/.pgpass
+
+psql "postgres://wdprcustomeradm@customers-postgres-12.c0ivpkyb3o26.eu-central-1.rds.amazonaws.com:5432/postgres" -c "select * from information_schema.schemata;"
 
 echo "Running Migrations"
 python ./manage.py migrate --no-input
