f51efe0d933d4b40c76802bc7975003d87b4e822
Anton Pirker <anton@ignaz.at>
2021-06-24 14:53:03 +0200

    f

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,10 @@ echo "printenv"
 printenv
 
 echo "Test Database"
-echo "postgres://antonpirker:e44ccd99e65b71bbaf13913e85e9063463fab1d3fd122d58692dc02dcede@postgres12.wdpr.run:5432/antonpirker"
-psql "postgres://antonpirker:e44ccd99e65b71bbaf13913e85e9063463fab1d3fd122d58692dc02dcede@postgres12.wdpr.run:5432/antonpirker" -c "select version();"
+#echo "postgres://antonpirker:e44ccd99e65b71bbaf13913e85e9063463fab1d3fd122d58692dc02dcede@postgres12.wdpr.run:5432/antonpirker"
+#psql "postgres://antonpirker:e44ccd99e65b71bbaf13913e85e9063463fab1d3fd122d58692dc02dcede@postgres12.wdpr.run:5432/antonpirker" -c "select version();"
+
+psql "postgres://wdprcustomeradm:%AqsI]IBvS[you[zK8z^=mOb-+gQ1$p<s}9=k%u}@customers-postgres-12.c0ivpkyb3o26.eu-central-1.rds.amazonaws.com:5432/postgres" -c "select * from information_schema.schemata;"
 
 echo "Running Migrations"
 python ./manage.py migrate --no-input
