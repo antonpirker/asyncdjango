3ecbefecbc11e19c5ebbaed4549a305bffb5cb2f
Anton Pirker <anton@ignaz.at>
2021-07-05 09:36:29 +0200

    Debugging

--- app/views.py
+++ app/views.py
@@ -19,9 +19,13 @@ def index(request, template_name="index.html"):
 
 def synchronous(request, template_name="_sync.html"):
     start_time = time.time()
+    logger.warn('sync 1')
     result = pokemon.get_pokemon_sync()
+    logger.warn('sync 2')
     pikachu = pokemon.get_one_pokemon_sync()
+    logger.warn('sync 3')
     duration = time.time() - start_time
+
 
     context = {
         'pokemon': pikachu,
@@ -34,10 +38,13 @@ def synchronous(request, template_name="_sync.html"):
 
 async def asynchronous(request, template_name="_async.html"):
     start_time = time.time()
+    logger.warn('async 1')
     result = await pokemon.get_pokemon_async()
+    logger.warn('async 2')
     pikachu = await pokemon.get_one_pokemon_async()
+    logger.warn('async 3')
     duration = time.time() - start_time
-
+    
     context = {
         'pokemon': pikachu,
         'pokemons': result,
